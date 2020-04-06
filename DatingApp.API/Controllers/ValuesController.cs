300d291a215ca4c9595bbd027be24bceb2372823
Junaid Tahir <junaid.mhtech@gmail.com>
2020-04-06 15:46:58 +0500

    Initail Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    //http://localhost:5000/api/values/
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context=context;
+        }
+        //http://localhost:5000/api/values/
+        /*Important Commands*/
+        // dotnet run - to run local server
+        // dotnet run watch -- to autosync change,not to rerun above code again and again // this is not working as expected in my case
+        // GET api/values
+        //[HttpGet]
+        /*public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }*/
+        [HttpGet]
+        //[EnableCors("CorsPolicy")]
+        public async Task<IActionResult> GetValues()
+        {
+           var values= await _context.Values.ToListAsync();
+           return Ok(values); 
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public  async Task<IActionResult> GetValue(int id)
+        {
+            var record= await _context.Values.FirstOrDefaultAsync(_vl=>_vl.id==id);
+            return Ok(record);
+            //return "value";
+        }
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Startup.cs
@@ -0,0 +1,60 @@
+﻿using DatingApp.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace DatingApp.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //var connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            // To Inject Dbcontect and load ConnectionString 
+              //services.AddCors();
+              services.AddDbContext<DataContext>(options =>options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+              services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // To enable cord add service a below
+             services.AddCors(options =>
+              {
+                    options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+              });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+               // app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            // To enable CORS
+            app.UseCors("CorsPolicy");
+
+            app.UseMvc();
+        }
+    }
+}
